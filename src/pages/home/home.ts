2ed476ed89e65c6c0198399daeb8e5a28af61a9f
Aton5859 <390072015@qq.com>
2019-11-13 17:59:16 +0800

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var echarts;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('EchartsContent') container: ElementRef;
+  EChart: any;
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad() {//当页面加载的时候触发，仅在页面创建的时候触发一次，如果被缓存了，那么下次再打开这个页面则不会触发
+    console.log('ionViewDidLoad AboutPage');
+    var data = [];
+    for (var i = 0; i <= 360; i++) {
+      var t = i / 180 * Math.PI;
+      var r = Math.sin(2 * t) * Math.cos(2 * t);
+      data.push([r, i]);
+    }
+
+    let ctelement = this.container.nativeElement;
+    this.EChart = echarts.init(ctelement);
+    this.EChart.setOption({
+      title: {
+        text: '极坐标双数值轴'
+      },
+      legend: {
+        data: ['line']
+      },
+      polar: {
+        center: ['50%', '54%']
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
+        }
+      },
+      angleAxis: {
+        type: 'value',
+        startAngle: 0
+      },
+      radiusAxis: {
+        min: 0
+      },
+      series: [{
+        coordinateSystem: 'polar',
+        name: 'line',
+        type: 'line',
+        showSymbol: false,
+        data: data
+      }],
+      animationDuration: 2000
+    });
+
+  }
 }
