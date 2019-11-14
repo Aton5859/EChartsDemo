593d8f5f552d9cf2ef917f0711309bc3bdc1ea03
Aton5859 <390072015@qq.com>
2019-11-14 15:49:21 +0800

    ionic segment组件使用尝试

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  pet: string = "puppies";
   constructor(public navCtrl: NavController) {
 
   }
